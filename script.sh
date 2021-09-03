c81c2a4ad121140a9e86a984aa9a57bf3e0b6708
Sintto <sintalj@gmail.com>
2021-09-03 20:56:05 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VarGit=$(curl https://raw.githubusercontent.com/Sintto/skusamGit/main/index.html | md5sum | cut -c1-32)
+VarLocal=$(md5sum /home/sinto/index.html | cut -c1-32)
+echo $VarGit
+if [[ $VarGit != $VarLocal ]]
+then
+        curl -O https://raw.githubusercontent.com/Sintto/skusamGit/main/index.html
+        echo "so zmenou"
+else
+        echo "bez zmeny"
+fi
